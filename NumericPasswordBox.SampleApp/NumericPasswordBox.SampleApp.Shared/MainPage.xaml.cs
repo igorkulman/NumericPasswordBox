7975efcf5fae7bf6c81aee8e719a80ed93c8ac45
Igor Kulman <igor@kulman.sk>
2015-05-16 21:49:05 +0200

    * sample app extended

--- NumericPasswordBox.SampleApp/NumericPasswordBox.SampleApp.Shared/MainPage.xaml.cs
+++ NumericPasswordBox.SampleApp/NumericPasswordBox.SampleApp.Shared/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,24 +26,13 @@ namespace NumericPasswordBox.SampleApp
         public MainPage()
         {
             this.InitializeComponent();
-
-            this.NavigationCacheMode = NavigationCacheMode.Required;
+            
         }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
+            var md = new MessageDialog(PasswordBox.Password);
+            md.ShowAsync();
         }
     }
 }
